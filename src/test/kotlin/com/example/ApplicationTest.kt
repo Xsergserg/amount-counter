b27ecbdfb3f8f16e1057df4ada0a5c2298c9655b
shapughin <xsergserg@gmail.com>
2024-01-08 19:45:59 +0100

    test: add application tests ✅

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -7,6 +7,11 @@ import com.example.plugins.configureSerialization
 import com.example.plugins.configureSwagger
 import com.example.service.ProductsService
 import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.matchers.shouldBe
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.http.HttpStatusCode.Companion.OK
+import io.ktor.server.testing.ApplicationTestBuilder
 import io.ktor.server.testing.testApplication
 import org.junit.jupiter.api.Test
 
@@ -26,5 +31,30 @@ class ApplicationTest {
                 }
             }
         }
+    }
+
+    @Test
+    fun `monitoring request returns OK(200) response`() {
+        testApplication {
+            testHttpClient().get("/metrics-micrometer").status shouldBe OK
+        }
+    }
+
+    @Test
+    fun `swager request returns OK(200) response`() {
+        testApplication {
+            testHttpClient().get("/swagger").status shouldBe OK
+        }
+    }
+
+    private fun ApplicationTestBuilder.testHttpClient(): HttpClient {
+        environment {
+            module {
+                configureMonitoring()
+                configureSwagger()
+                configureSerialization()
+            }
+        }
+        return createClient {}
     }
 }
