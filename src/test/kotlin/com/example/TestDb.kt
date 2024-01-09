963458e84f82fa7158d543286d3049868db93a5f
shapughin <xsergserg@gmail.com>
2024-01-09 07:20:27 +0100

    refactor: add explicit id declaration for insert rows ♻️

--- src/test/kotlin/com/example/TestDb.kt
+++ src/test/kotlin/com/example/TestDb.kt
@@ -19,22 +19,27 @@ fun initTestDb(db: Database) {
     transaction(db) {
         SchemaUtils.create(ProductsTable)
         ProductsTable.insert {
+            it[id] = 1L
             it[name] = "Diablo IV"
             it[amount] = 10L
         }
         ProductsTable.insert {
+            it[id] = 2L
             it[name] = "Overwatch 2"
             it[amount] = 1L
         }
         ProductsTable.insert {
+            it[id] = 3L
             it[name] = "Half-Life 3"
             it[amount] = 10000L
         }
         ProductsTable.insert {
+            it[id] = 4L
             it[name] = "CyberPunk 2077"
             it[amount] = 50L
         }
         ProductsTable.insert {
+            it[id] = 5L
             it[name] = "God of War: Ragnarok"
             it[amount] = 5L
         }
