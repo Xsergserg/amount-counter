8d8d3b7666b8fa58e07b65415f5a2e214771babc
shapughin <xsergserg@gmail.com>
2024-01-09 07:19:56 +0100

    fix: table column name fix 🐛

--- src/main/kotlin/com/example/db/ProductsTable.kt
+++ src/main/kotlin/com/example/db/ProductsTable.kt
@@ -7,7 +7,7 @@ private const val VARCHAR_LENGTH = 256
 object ProductsTable : Table("Products") {
     val id = long("id").uniqueIndex().autoIncrement()
     val name = varchar("name", VARCHAR_LENGTH)
-    val amount = long("age")
+    val amount = long("amount")
 
     override val primaryKey = PrimaryKey(id, name = "PK_Products_Id")
 }
