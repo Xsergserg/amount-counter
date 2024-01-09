c0438b7ec97be4376358d346455eb24ffd736cfd
shapughin <xsergserg@gmail.com>
2024-01-09 07:14:51 +0100

    test: add testDb utils ✅

--- src/test/kotlin/com/example/TestDb.kt
+++ src/test/kotlin/com/example/TestDb.kt
@@ -1,6 +1,10 @@
 package com.example
 
+import com.example.db.ProductsTable
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun getTestDb(): Database {
     return Database.connect(
@@ -9,4 +13,30 @@ fun getTestDb(): Database {
         driver = "org.h2.Driver",
         password = "test",
     )
+}
+
+fun initTestDb(db: Database) {
+    transaction(db) {
+        SchemaUtils.create(ProductsTable)
+        ProductsTable.insert {
+            it[name] = "Diablo IV"
+            it[amount] = 10L
+        }
+        ProductsTable.insert {
+            it[name] = "Overwatch 2"
+            it[amount] = 1L
+        }
+        ProductsTable.insert {
+            it[name] = "Half-Life 3"
+            it[amount] = 10000L
+        }
+        ProductsTable.insert {
+            it[name] = "CyberPunk 2077"
+            it[amount] = 50L
+        }
+        ProductsTable.insert {
+            it[name] = "God of War: Ragnarok"
+            it[amount] = 5L
+        }
+    }
 }
