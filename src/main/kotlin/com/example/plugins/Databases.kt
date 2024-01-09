efb318a1d54ea8ff3ce9f82beac07d6679e255c6
shapughin <xsergserg@gmail.com>
2024-01-09 07:06:43 +0100

    feat: add initial data to db 🗃️

--- src/main/kotlin/com/example/plugins/Databases.kt
+++ src/main/kotlin/com/example/plugins/Databases.kt
@@ -3,6 +3,7 @@ package com.example.plugins
 import com.example.db.ProductsTable
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun configureDatabases(): Database {
@@ -14,8 +15,29 @@ fun configureDatabases(): Database {
     ).also { initDb(it) }
 }
 
+@Suppress("MagicNumber")
 fun initDb(db: Database) {
     transaction(db) {
         SchemaUtils.create(ProductsTable)
+        ProductsTable.insert {
+            it[name] = "Diablo IV"
+            it[amount] = 10L
+        }
+        ProductsTable.insert {
+            it[name] = "Overwatch 2"
+            it[amount] = 1L
+        }
+        ProductsTable.insert {
+            it[name] = "Half-Life 3"
+            it[amount] = 10000L
+        }
+        ProductsTable.insert {
+            it[name] = "CyberPunk 2077"
+            it[amount] = 50L
+        }
+        ProductsTable.insert {
+            it[name] = "God of War: Ragnarok"
+            it[amount] = 5L
+        }
     }
 }
